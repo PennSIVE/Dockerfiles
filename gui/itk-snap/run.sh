082ef77e7ccd7fc83935b714eeca2b1c8ac3e33f
Tim <tim.terf@gmail.com>
2020-05-30 13:32:25 -0400

    containerized itk-snap

---
+++ gui/itk-snap/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# remember to add connection to acl with xhost, get display IP with `ifconfig en0 | grep 'inet '`
+docker run --rm -ti -e QT_X11_NO_MITSHM="1" -e DISPLAY="192.168.0.148:0" \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    pennsive/itk-snap:latest
+
