cfa46f786ef70a96d9a7a1d6070318904e5b22eb
Tim <tim.terf@gmail.com>
2020-08-05 21:01:44 -0400

    created another neurodebian base image (with ants) but also moving to neurodocker approach

---
+++ neurodocker/buster.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker run --rm repronim/neurodocker:0.7.0 generate docker \
+    --pkg-manager apt \
+    --base debian:buster \
+    --run "apt-get update && apt-get install -y multiarch-support" \
+    --afni version=latest \
+    --ants version=2.3.1 \
+    --fsl version=6.0.3 \
+    --minc version=1.9.15 \
+    | docker build -t pennsive/neurodocker:buster -
+# note: without multiarch-support dpkg will not be able to install the pre-dependancy libxp6
