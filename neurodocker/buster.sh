0c8a1fb7632aff6a0b7923f03911b38d8d12c118
Tim <tim.terf@gmail.com>
2020-08-18 22:18:14 -0400

    added freesurfer and c3d to neurodocker image; added some more packages to neuroR

--- neurodocker/buster.sh
+++ neurodocker/buster.sh
@@ -8,5 +8,7 @@ docker run --rm repronim/neurodocker:0.7.0 generate docker \
     --ants version=2.3.1 \
     --fsl version=6.0.3 \
     --minc version=1.9.15 \
+    --freesurfer version=6.0.0 \
+    --convert3d version=1.0.0 \
     | docker build -t pennsive/neurodocker:buster -
 # note: without multiarch-support dpkg will not be able to install the pre-dependancy libxp6
