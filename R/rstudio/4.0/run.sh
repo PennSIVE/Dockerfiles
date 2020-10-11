4283ba3958b64ae17eecc112a6b9128ba42db29d
Tim <tim.terf@gmail.com>
2020-10-11 11:53:12 -0400

    some special things required to get rstudio to work with singularity

--- R/rstudio/4.0/run.sh
+++ R/rstudio/4.0/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker run --entrypoint="" -it -e DISABLE_AUTH=true -v $HOME:/data -w /data -p 80:8787 pennsive/rstudio:4.0 bash
-# singularity run --cleanenv --pid -B $TMPDIR:/var/run/rstudio-server rstudio_4.0.sif
+docker run --rm -d -e PORT=9090 -v $HOME:/data -w /data -p 80:9090 pennsive/rstudio:4.0
+# ssh -L12345:127.0.0.1:12345 timrf@takim
+# SINGULARITYENV_PORT=12345 singularity run --cleanenv -B $TMPDIR:/var/run/rstudio-server /project/taki3/simg/rstudio_4.0.sif
