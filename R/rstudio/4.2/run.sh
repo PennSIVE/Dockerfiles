4d527cc9cf50a3dd85c3c51540c341584aeb9647
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2022-12-06 16:50:10 -0500

    Update RStudio to R version 4.2

---
+++ R/rstudio/4.2/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker run --rm -d -e PORT=9090 -v $HOME:/data -w /data -p 80:9090 pennsive/rstudio:4.2
+# ssh -L12345:127.0.0.1:12345 timrf@takim
+# SINGULARITYENV_PORT=12345 singularity run --cleanenv -B $TMPDIR:/var/run/rstudio-server /project/taki3/simg/rstudio_4.0.sif
