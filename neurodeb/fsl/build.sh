9071cb86a2a3bbcc5456d259b6544b265fd9d9c3
Virgilio Gonzenbach <62161049+vgonzenbach@users.noreply.github.com>
2023-02-15 09:50:59 -0500

    switch to neurodocker method

--- neurodeb/fsl/build.sh
+++ neurodeb/fsl/build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker build -t pennsive/fsl .
+# to generate Dockerfile
+#docker run --rm kaczmarj/neurodocker:0.9.1 generate docker \
+#    --pkg-manager apt \
+#    --base-image neurodebian:buster \
+#    --fsl version=6.0.5 > Dockerfile
+docker build -t pennsive/fsl:6.0.5 . &> build.log 
